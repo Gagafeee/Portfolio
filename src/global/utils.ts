52ef94650df8c56cc657533e4d744dd380a39171
Gagafeee <Gabriel.courroux@lilo.org>
2025-07-15 15:35:22 +0200

    Base rewrite
    - Cleared all files
    - Added project's fundamentals

--- src/global/utils.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-const isServer = typeof window === "undefined";
-export function vwToPx(value: number): number {
-    return !isServer ? Math.floor((window.innerWidth * value) / 100) : 0;
-}
-
-export function vhToPx(value: number): number {
-    return !isServer ? Math.floor((window.innerHeight * value) / 100) : 0;
-}
