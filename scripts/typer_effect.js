09a36cb7c2e9a45afb47a62a9ef1882b1e27fcd6
Gagafeee <64372089+Gagafeee@users.noreply.github.com>
2025-07-21 18:59:15 +0200

    Fixes
    - Fixed horizontal scrolling on chromium.
    - Fixed layout issue

--- scripts/typer_effect.js
+++ scripts/typer_effect.js
@@ -1,12 +1,14 @@
 // Initialize Hero banner typing effect
 const titleSelector = "#hero .container .title";
 if (document.querySelector(titleSelector) == null) throw new Error("Cannot initialize typer effect on hero section: Node not found");
-else document.querySelector(titleSelector).innerHTML = "";
-const typingEffect = new Typed(titleSelector, {
-    strings: ["Je crée. ^2000", "Je design.", "J'imagine.", "Je conçois.", "J'optimise."],
-    typeSpeed: 70,
-    startDelay: 70,
-    backSpeed: 60,
-    backDelay: 1500,
-    loop: true,
-});
+
+setTimeout(() => {
+    document.querySelector(titleSelector).innerHTML = "";
+    new Typed(titleSelector, {
+        strings: ["Je crée. ^2000", "Je design.", "J'imagine.", "Je conçois.", "J'optimise."],
+        typeSpeed: 70,
+        backSpeed: 60,
+        backDelay: 1500,
+        loop: true,
+    });
+}, 70)
