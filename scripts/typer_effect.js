b3454844328f444438e8926a8bb354316606552b
Gagafeee <64372089+Gagafeee@users.noreply.github.com>
2025-07-21 19:54:43 +0200

    Global review
    - Changed glassy colors.
    - Changed project illustration transition.
    - Reviewed css vars

--- scripts/typer_effect.js
+++ scripts/typer_effect.js
@@ -1,10 +1,10 @@
 // Initialize Hero banner typing effect
-const titleSelector = "#hero .container .title";
-if (document.querySelector(titleSelector) == null) throw new Error("Cannot initialize typer effect on hero section: Node not found");
+const titleLocator = "#hero .card .title";
+if (document.querySelector(titleLocator) == null) throw new Error("Cannot initialize typer effect on the hero section: Node not found. Locator: " + titleLocator);
 
 setTimeout(() => {
-    document.querySelector(titleSelector).innerHTML = "";
-    new Typed(titleSelector, {
+    document.querySelector(titleLocator).innerHTML = "";
+    new Typed(titleLocator, {
         strings: ["Je crée. ^2000", "Je design.", "J'imagine.", "Je conçois.", "J'optimise."],
         typeSpeed: 70,
         backSpeed: 60,
